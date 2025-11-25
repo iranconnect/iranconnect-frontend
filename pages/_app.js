0a430152c55c0cfa21a916afbd7bceb661195815
admin-iranconnect <admin@iranconnect.org>
2025-11-25 14:14:47 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -106,7 +106,7 @@ export default function App({ Component, pageProps }) {
 
     setIsLoggedIn(false);
     setInactive(false);
-    router.push('/auth/login');
+    router.replace('/auth/login');
   }
 
   /* 🙋 ادامه حضور */
