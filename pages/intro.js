b48c84947800677ba9d9af0c97ad9f50c2a93019
admin-iranconnect <admin@iranconnect.org>
2025-12-01 20:20:56 +0100

    Update intro.js

--- pages/intro.js
+++ pages/intro.js
@@ -10,7 +10,7 @@ export default function Intro() {
       setTimeout(() => setStep(2), 4000), // پیام خوش‌آمد طولانی‌تر
       setTimeout(() => setStep(3), 6500), // نمایش لوگو از opacity 0→1
       setTimeout(() => setStep(4), 8000), // نمایش جدول دکمه‌ها
-    ];
+    ]; 
     return () => timers.forEach(clearTimeout);
   }, []);
 
