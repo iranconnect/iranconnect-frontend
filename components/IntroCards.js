8b631420c9ad97c17f30bf1790e9c25fee537c39
admin-iranconnect <admin@iranconnect.org>
2025-12-01 13:26:32 +0100

    Update IntroCards.js

--- components/IntroCards.js
+++ components/IntroCards.js
@@ -9,76 +9,70 @@ export default function IntroCards() {
       title: "About Us",
       gif: "/animation4.gif",
       icon: "/icons/about.svg",
-      delayClass: "card-1",
       small: true,
+      delay: "card-1",
       onClick: () => router.push("/about"),
     },
     {
       title: "Health & Medicine",
       gif: "/animation1.gif",
       icon: "/icons/health.svg",
-      delayClass: "card-2",
+      delay: "card-2",
       onClick: () => router.push("/?category=doctor&limit=10"),
     },
     {
       title: "Translate & Interpreter",
       gif: "/animation2.gif",
       icon: "/icons/translate.svg",
-      delayClass: "card-3",
+      delay: "card-3",
       onClick: () => router.push("/?category=language-services&limit=10"),
     },
     {
       title: "Lawyer",
       gif: "/animation3.gif",
       icon: "/icons/lawyer.svg",
-      delayClass: "card-4",
+      delay: "card-4",
       onClick: () => router.push("/?category=lawyer&limit=10"),
     },
   ];
 
   return (
-    <div className="intro-cards-area">
+    <div className="intro-cards-wrapper">
 
       {/* نسخه دسکتاپ */}
       <div className="intro-cards-desktop">
         {cards.map((card, index) => (
           <div
             key={index}
-            className={`intro-card slide-up ${card.delayClass} ${
-              card.small ? "card-small" : ""
-            }`}
+            className={`intro-card slide-up ${card.delay} ${card.small ? "card-small" : ""}`}
             onClick={card.onClick}
           >
             <div className="intro-card-top">
-              <img className="intro-card-gif" src={card.gif} alt={card.title} />
+              <img src={card.gif} className="intro-card-gif" />
             </div>
 
             <div className="intro-card-middle">
-              <img className="intro-card-icon" src={card.icon} alt={card.title} />
+              <img src={card.icon} className="intro-card-icon" />
             </div>
 
-            <div className="intro-card-bottom">
-              <p>{card.title}</p>
-            </div>
+            <p className="intro-card-title">{card.title}</p>
           </div>
         ))}
       </div>
 
-      {/* نسخه موبایل — اسکرول افقی */}
+      {/* نسخه موبایل */}
       <div className="intro-cards-mobile">
-        <div className="mobile-scroll">
-          {cards.map((card, index) => (
-            <div
-              key={index}
-              className={`intro-card-mobile slide-up ${card.delayClass}`}
-              onClick={card.onClick}
-            >
-              <img className="intro-card-mobile-gif" src={card.gif} />
-              <img className="intro-card-mobile-icon" src={card.icon} />
-              <p className="intro-card-mobile-title">{card.title}</p>
-            </div>
-          ))}
-        </div>
+        {cards.map((card, index) => (
+          <div
+            key={index}
+            className={`intro-card-mobile slide-up ${card.delay}`}
+            onClick={card.onClick}
+          >
+            <img className="intro-card-mobile-gif" src={card.gif} />
+            <img className="intro-card-mobile-icon" src={card.icon} />
+            <p className="intro-card-mobile-title">{card.title}</p>
+          </div>
+        ))}
       </div>
 
     </div>
