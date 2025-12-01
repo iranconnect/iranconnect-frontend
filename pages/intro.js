3d6f8300d3df6024b03a39c82d2455f3e766bfcd
admin-iranconnect <admin@iranconnect.org>
2025-12-01 21:08:24 +0100

    Update intro.js

--- pages/intro.js
+++ pages/intro.js
@@ -1,6 +1,7 @@
 //frontend/pages/intro.js
 import { useEffect, useState } from "react";
 import IntroCards from "../components/IntroCards";
+import Footer from "../components/Footer";  // اضافه کردن فوتر
 
 export default function Intro() {
   const [step, setStep] = useState(1);
@@ -10,7 +11,7 @@ export default function Intro() {
       setTimeout(() => setStep(2), 4000), // پیام خوش‌آمد طولانی‌تر
       setTimeout(() => setStep(3), 6500), // نمایش لوگو از opacity 0→1
       setTimeout(() => setStep(4), 8000), // نمایش جدول دکمه‌ها
-    ]; 
+    ];
     return () => timers.forEach(clearTimeout);
   }, []);
 
@@ -29,6 +30,8 @@ export default function Intro() {
         <IntroCards showLogo={true} showButtons={step === 4} />
       )}
 
+      {/* اضافه کردن فوتر به صفحه Intro */}
+      <Footer />
     </div>
   );
 }
