482a6e5098007d6446dac6276829f8d9b6a6cf6e
admin-iranconnect <admin@iranconnect.org>
2026-01-03 22:56:47 +0100

    Update StepMediaReview.jsx

--- components/admin/BusinessWizard/StepMediaReview.jsx
+++ components/admin/BusinessWizard/StepMediaReview.jsx
@@ -423,6 +423,7 @@ export default function StepMediaReview({
         }
 
         resetBusyState();
+        setGalleryPreview([]);
       } catch (err) {
           setGalleryPreview((prev) =>
             prev.map((item) =>
