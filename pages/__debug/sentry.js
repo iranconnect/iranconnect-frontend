7def394571af545b32eba7e98de60d6506cb3243
admin-iranconnect <admin@iranconnect.org>
2026-01-08 00:55:55 +0100

    Update sentry.js

--- pages/__debug/sentry.js
+++ pages/__debug/sentry.js
@@ -1,10 +1,12 @@
-//frontend/pages/__debug/sentry.js
-import { useEffect } from "react";
-
+// pages/__debug/sentry.js
 export default function SentryTest() {
-  useEffect(() => {
-    throw new Error("🔥 FRONTEND_SENTRY_STAGING_TEST");
-  }, []);
-
-  return <div>Sentry test page</div>;
+  return (
+    <button
+      onClick={() => {
+        throw new Error("🔥 FRONTEND_SENTRY_STAGING_TEST");
+      }}
+    >
+      Trigger Sentry Error
+    </button>
+  );
 }
