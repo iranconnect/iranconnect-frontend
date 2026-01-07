e386ae0554a050b1ab9fcfa8140ce0b51d6b531b
admin-iranconnect <admin@iranconnect.org>
2026-01-07 23:55:49 +0100

    Create sentry.server.config.js

---
+++ sentry.server.config.js
@@ -0,0 +1,12 @@
+//frontend/sentry.server.config.js
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+
+  environment: process.env.NEXT_PUBLIC_ENV,
+
+  tracesSampleRate: 0.1,
+
+  enabled: true,
+});
