1496cf42f2c5c17bac896d348c9f3547199a0940
admin-iranconnect <admin@iranconnect.org>
2026-01-08 13:33:32 +0100

    Update add-v2.js

--- pages/admin/add-v2.js
+++ pages/admin/add-v2.js
@@ -1,15 +1,17 @@
 //pages/admin/add-v2.js
+import { useEffect } from "react"; // for test Sentry
+import * as Sentry from "@sentry/nextjs"; // for test Sentry
 import AdminLayout from "../../components/admin/AdminLayout";
 import BusinessWizard from "../../components/admin/BusinessWizard";
 import { useSentryPageTags } from "../../hooks/useSentryPageTags";
+
 
 export default function AddBusinessV2() {
   useSentryPageTags({
     feature: "business-wizard-v2",
   });
 
-  useSentryPageTags({ feature: "business-wizard-v2" });
-
+  // for test Sentry
   useEffect(() => {
     Sentry.captureMessage("ADD_V2_PAGE_LOADED_TEST");
   }, []);
