913dfcecbe91bd0bc8837503d8b6aaafb445b276
admin-iranconnect <admin@iranconnect.org>
2026-01-08 11:21:54 +0100

    Update add-v2.js

--- pages/admin/add-v2.js
+++ pages/admin/add-v2.js
@@ -1,8 +1,13 @@
 //pages/admin/add-v2.js
 import AdminLayout from "../../components/admin/AdminLayout";
 import BusinessWizard from "../../components/admin/BusinessWizard";
+import { useSentryPageTags } from "../../hooks/useSentryPageTags";
 
 export default function AddBusinessV2() {
+  useSentryPageTags({
+    feature: "business-wizard-v2",
+  });
+
   return (
     <AdminLayout>
       <main className="admin-container">
