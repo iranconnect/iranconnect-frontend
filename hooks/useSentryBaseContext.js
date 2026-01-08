fe55d00fec345887167b0cdc73add29caa5e3381
admin-iranconnect <admin@iranconnect.org>
2026-01-08 16:20:12 +0100

    Update useSentryBaseContext.js

--- hooks/useSentryBaseContext.js
+++ hooks/useSentryBaseContext.js
@@ -1,17 +1,24 @@
 //frontend/hooks/useSentryBaseContext.js
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { setSentryBaseContext } from "../utils/sentryContext";
 
 export function useSentryBaseContext({ role }) {
   const router = useRouter();
+  const [ready, setReady] = useState(false);
 
   useEffect(() => {
     if (!router.pathname) return;
+    if (!role) return;
 
     setSentryBaseContext({
-      role: role || "guest",
+      role,                // ❗ دیگر guest نداریم
       page: router.pathname,
     });
+
+    setReady(true);
   }, [role, router.pathname]);
+
+  return ready;
 }
+
