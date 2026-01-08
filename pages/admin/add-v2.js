02e78f06b9111fd3cf7f9dbd05b3c8e81b5e83f1
admin-iranconnect <admin@iranconnect.org>
2026-01-08 16:48:02 +0100

    Update add-v2.js

--- pages/admin/add-v2.js
+++ pages/admin/add-v2.js
@@ -1,23 +1,23 @@
 //pages/admin/add-v2.js
-import { useEffect } from "react"; // for test Sentry
-import * as Sentry from "@sentry/nextjs"; // for test Sentry
+import { useEffect } from "react";
+import * as Sentry from "@sentry/nextjs";
 import AdminLayout from "../../components/admin/AdminLayout";
 import BusinessWizard from "../../components/admin/BusinessWizard";
-import { useSentryPageTags } from "../../hooks/useSentryPageTags";
 import { useSentryPageFeature } from "../../hooks/useSentryPageFeature";
 
-
 export default function AddBusinessV2() {
-  const withFeature = useSentryPageFeature("business-wizard-v2");
+  const { withFeature, sentryReady } =
+    useSentryPageFeature("business-wizard-v2");
 
   useEffect(() => {
+    if (!sentryReady) return;
+
     withFeature(() => {
       Sentry.captureMessage("ADD_V2_PAGE_LOADED", {
         level: "info",
       });
     });
-  }, []);
-
+  }, [sentryReady]);
 
   return (
     <AdminLayout>
