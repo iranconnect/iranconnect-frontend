34461b65452ca09c55ef919a0692e36e0e45094a
admin-iranconnect <admin@iranconnect.org>
2026-01-08 17:07:11 +0100

    Delete sentry.edge.config.js

--- sentry.edge.config.js
+++
@@ -1,8 +0,0 @@
-//frontend/sentry.edge.config.js
-import * as Sentry from "@sentry/nextjs";
-
-Sentry.init({
-  dsn: process.env.SENTRY_DSN,
-  environment: process.env.NEXT_PUBLIC_ENV,
-  tracesSampleRate: 0.1,
-});
