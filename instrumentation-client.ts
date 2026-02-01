ecd3ccafb5fbba41ec3fec255c6abc728bae7a47
admin-iranconnect <admin@iranconnect.org>
2026-02-01 20:00:40 +0100

    Update instrumentation-client.ts

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -2,30 +2,30 @@
 import * as Sentry from "@sentry/nextjs";
 
 export function register() {
-  Sentry.init({
-    dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-    environment: process.env.NEXT_PUBLIC_ENV || "development",
+  //Sentry.init({
+    //dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+    //environment: process.env.NEXT_PUBLIC_ENV || "development",
 
-    tracesSampleRate: 0.2,
+    //tracesSampleRate: 0.2,
 
-    integrations: [
-      Sentry.browserTracingIntegration(),
-      Sentry.replayIntegration(),
-    ],
+    //integrations: [
+      //Sentry.browserTracingIntegration(),
+      //Sentry.replayIntegration(),
+    //],
 
-    beforeSend(event) {
-    // Always allow errors
-    if (event.level === "error" || event.exception) {
-      return event;
-    }
+    //beforeSend(event) {
+    //// Always allow errors
+    //if (event.level === "error" || event.exception) {
+      //return event;
+    //}
   
-    // Drop ONLY non-error events without page
-    if (!event.tags?.page) {
-      return null;
-    }
+    //// Drop ONLY non-error events without page
+    //if (!event.tags?.page) {
+      //return null;
+    //}
   
-    return event;
-  },
+    //return event;
+  //},
 
-  });
+  //});
 }
