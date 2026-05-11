1a67272ce15be7e391cbcd83eb215b4d94b52641
admin-iranconnect <admin@iranconnect.org>
2026-05-11 18:32:07 +0200

    Update requests.js

--- pages/account/requests.js
+++ pages/account/requests.js
@@ -112,7 +112,7 @@ export default function RequestHistory() {
 
   return (
     <AccountLayout>
-      <main className="flex-1 px-4 py-10 w-full max-w-6xl mx-auto">
+      <main className="mx-auto w-full max-w-5xl">
         <div className="w-full">
         
           <h1
