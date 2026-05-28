41e2d2ce44fc47e562fbe10b9edd334bac4cf7d3
admin-iranconnect <admin@iranconnect.org>
2026-05-28 22:41:48 +0200

    Create featuredBusinesses.js

---
+++ data/featuredBusinesses.js
@@ -0,0 +1,37 @@
+//frontend/data/featuredBusinesses.js
+const featuredBusinesses = [
+  {
+    id: 1,
+    slug: "dr-sara-mohammadi",
+    name: "Dr. Sara Mohammadi",
+    category: "Doctors",
+    city: "Toronto",
+    country: "Canada",
+    avg_rating: 4.9,
+    logo_url: "/logo-light.png",
+  },
+
+  {
+    id: 2,
+    slug: "paris-persian-law-group",
+    name: "Paris Persian Law Group",
+    category: "Lawyers",
+    city: "Paris",
+    country: "France",
+    avg_rating: 4.8,
+    logo_url: "/logo-light.png",
+  },
+
+  {
+    id: 3,
+    slug: "nice-translation-center",
+    name: "Nice Translation Center",
+    category: "Translators",
+    city: "Nice",
+    country: "France",
+    avg_rating: 4.7,
+    logo_url: "/logo-light.png",
+  },
+];
+
+export default featuredBusinesses;
