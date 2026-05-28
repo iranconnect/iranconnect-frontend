3135ee54e8c90f651dacb96892027b0b76a0a718
admin-iranconnect <admin@iranconnect.org>
2026-05-28 21:37:25 +0200

    Create featuredCategories.js

---
+++ data/featuredCategories.js
@@ -0,0 +1,36 @@
+//frontend/data/featuredCategories.js
+const featuredCategories = [
+  {
+    title: "Doctors",
+    slug: "doctors",
+    description:
+      "Find trusted Iranian doctors and healthcare professionals.",
+    icon: "/icons/health.svg",
+  },
+
+  {
+    title: "Lawyers",
+    slug: "lawyers",
+    description:
+      "Connect with experienced Iranian lawyers and legal experts.",
+    icon: "/icons/lawyer.svg",
+  },
+
+  {
+    title: "Translators",
+    slug: "translators",
+    description:
+      "Professional Persian translators and interpretation services.",
+    icon: "/icons/translate.svg",
+  },
+
+  {
+    title: "Community Services",
+    slug: "community-services",
+    description:
+      "Discover local Iranian-owned businesses and services.",
+    icon: "/icons/about.svg",
+  },
+];
+
+export default featuredCategories;
