62dcc4587ddee51225c3402e24556054f8e92645
admin-iranconnect <admin@iranconnect.org>
2026-05-28 22:17:53 +0200

    Create popularCities.js

---
+++ data/popularCities.js
@@ -0,0 +1,46 @@
+//frontend/data/popularCities.js
+const popularCities = [
+  {
+    city: "Toronto",
+    country: "Canada",
+    slug: "toronto",
+    businesses: "40+ businesses",
+  },
+
+  {
+    city: "Paris",
+    country: "France",
+    slug: "paris",
+    businesses: "25+ businesses",
+  },
+
+  {
+    city: "Nice",
+    country: "France",
+    slug: "nice",
+    businesses: "20+ businesses",
+  },
+
+  {
+    city: "Los Angeles",
+    country: "USA",
+    slug: "los-angeles",
+    businesses: "18+ businesses",
+  },
+
+  {
+    city: "Vancouver",
+    country: "Canada",
+    slug: "vancouver",
+    businesses: "14+ businesses",
+  },
+
+  {
+    city: "Berlin",
+    country: "Germany",
+    slug: "berlin",
+    businesses: "12+ businesses",
+  },
+];
+
+export default popularCities;
