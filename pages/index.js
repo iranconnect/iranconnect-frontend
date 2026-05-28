508ece28f39c1c3397c9ffbf91ccc689ab5e618a
admin-iranconnect <admin@iranconnect.org>
2026-05-28 21:40:04 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
 //iranconnect-frontend/pages/index.js
-
 import Layout from "../components/Layout";
+
 import HeroSection from "../components/home/HeroSection";
+import FeaturedCategories from "../components/home/FeaturedCategories";
 
 export default function HomePage() {
   return (
     <Layout>
       <HeroSection />
+
+      <FeaturedCategories />
     </Layout>
   );
 }
