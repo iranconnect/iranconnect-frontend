a1ae20ac5ce29351caf599b2cf4451477772d070
admin-iranconnect <admin@iranconnect.org>
2026-05-31 22:08:27 +0200

    Update CategoryCard.jsx

--- components/home/CategoryCard.jsx
+++ components/home/CategoryCard.jsx
@@ -25,7 +25,7 @@ export default function CategoryCard({
         <div
           className="
             relative
-            h-36
+            h-52
             -mx-6
             -mt-6
             mb-5
