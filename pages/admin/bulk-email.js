4bca895ed552b8b59f3120c4d1b5b021763a7f9e
admin-iranconnect <admin@iranconnect.org>
2026-07-07 08:02:52 +0200

    Update bulk-email.js

--- pages/admin/bulk-email.js
+++ pages/admin/bulk-email.js
@@ -584,7 +584,7 @@ export default function BulkEmailPage() {
             </div>
 
             <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 mt-6">
-              <div className="quill-wrapper bulk-email-quill-wrapper relative">
+              <div className="quill-wrapper bulk-email-quill-wrapper">
                 <label className="block mb-2 text-sm font-medium">
                   Email Content
                 </label>
