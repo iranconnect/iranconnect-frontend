503bbb4aaa49dcf8245dfdf7ef10743e6eca2ac5
admin-iranconnect <admin@iranconnect.org>
2026-07-31 07:21:42 +0000

    security(sentry): restrict debug route to staging

--- pages/__debug/sentry.js
+++ pages/__debug/sentry.js
@@ -1,12 +1,27 @@
 // pages/__debug/sentry.js
+
 export default function SentryTest() {
   return (
     <button
       onClick={() => {
-        throw new Error("🔥 FRONTEND_SENTRY_STAGING_TEST");
+        throw new Error(
+          "🔥 FRONTEND_SENTRY_STAGING_TEST"
+        );
       }}
     >
       Trigger Sentry Error
     </button>
   );
+}
+
+export function getServerSideProps() {
+  if (process.env.NEXT_PUBLIC_ENV !== "staging") {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {},
+  };
 }
